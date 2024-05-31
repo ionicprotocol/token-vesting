ae82ddb49ca694c374bef2530a01854765a8cf41
Rahul Sethuram <rahul.eth@icloud.com>
2024-05-31 16:29:57 +0200

    feat: vesting setup

--- script/setVestingAmounts.ts
+++ script/setVestingAmounts.ts
@@ -12,9 +12,10 @@ type Row = {
   vested_amount: string;
 };
 
-const TOKEN_VESTING_MODE = "";
+const TOKEN_VESTING_MODE = "0x93E63535CB8b85239D4D8F40a571e81dAb9409d9";
 
 async function main() {
+  const client = await hre.viem.getPublicClient();
   const file = fs.readFileSync(`airdrop-amounts-final.csv`);
   const csv = parse(file, {
     columns: true,
@@ -35,15 +36,19 @@ async function main() {
   });
   console.log("num batches: ", batches.length);
   console.log("sum: ", formatEther(sum));
+  console.log("sum raw: ", sum);
   const tokenVesting = await hre.viem.getContractAt(
     "TokenVesting",
     TOKEN_VESTING_MODE
   );
   for (const batch of batches) {
-    await tokenVesting.write.setVestingAmounts([
+    const tx = await tokenVesting.write.setVestingAmounts([
+      batch.reduce((sum, row) => (sum += BigInt(row.vested_amount)), 0n),
       batch.map((row) => row.user),
       batch.map((row) => BigInt(row.vested_amount)),
     ]);
+    const receipt = await client.waitForTransactionReceipt({ hash: tx });
+    console.log("receipt.transactionHash: ", receipt.transactionHash);
   }
 }
 
