b6d868071a6d88c7e72239c13a90990a71124810
Rahul Sethuram <rahul.eth@icloud.com>
2024-07-09 11:20:09 +0400

    feat: airdrop

--- script/setVestingAmountsExtendedSnapshot.ts
+++ script/setVestingAmountsExtendedSnapshot.ts
@@ -1,6 +1,6 @@
 import { parse } from "csv-parse/sync";
 import hre from "hardhat";
-import fs from "fs";
+import { readFileSync, writeFileSync } from "fs";
 import { Address, formatEther } from "viem";
 
 const BATCH_SIZE = 1000;
@@ -16,7 +16,7 @@ const TOKEN_VESTING_MODE = "0xa7BC89F9Bcd2E6565c250182767f20e2aC89bc7B";
 
 async function main() {
   const client = await hre.viem.getPublicClient();
-  const file = fs.readFileSync(`airdrop-amounts-final-batch3.csv`);
+  const file = readFileSync(`airdrop-amounts-final-extended.csv`);
   const csv = parse(file, {
     columns: true,
     skip_empty_lines: true,
@@ -38,13 +38,13 @@ async function main() {
     batches.push(batch);
   }
   console.log("num batches: ", batches.length);
-  console.log("sum: ", formatEther(sum));
-  console.log("sum raw: ", sum);
   const tokenVesting = await hre.viem.getContractAt(
     "TokenVesting",
     TOKEN_VESTING_MODE
   );
   let i = 1;
+  const headerRow = "user,total_amount,liquid_amount,vested_amount\n";
+  const deduped: Row[] = [];
   for (const batch of batches) {
     const vests = await Promise.all(
       batch.map(async (row) => {
@@ -53,21 +53,45 @@ async function main() {
       })
     );
     const _batch = vests.filter((row) => row.vestAmount === 0n);
+    console.log(
+      `Processing batch ${i} of ${batches.length} batches: Batch Length - ${_batch.length}`
+    );
     // const _batch = batch;
     if (_batch.length > 0) {
-      const tx = await tokenVesting.write.setVestingAmounts([
-        _batch.reduce((sum, row) => (sum += BigInt(row.vested_amount)), 0n),
-        _batch.map((row) => row.user),
-        _batch.map((row) => BigInt(row.vested_amount)),
-      ]);
-      const receipt = await client.waitForTransactionReceipt({ hash: tx });
-      console.log("receipt.transactionHash: ", receipt.transactionHash);
+      deduped.push(..._batch);
+      // const tx = await tokenVesting.write.setVestingAmounts([
+      //   _batch.reduce((sum, row) => (sum += BigInt(row.vested_amount)), 0n),
+      //   _batch.map((row) => row.user),
+      //   _batch.map((row) => BigInt(row.vested_amount)),
+      // ]);
+      // const receipt = await client.waitForTransactionReceipt({ hash: tx });
+      // console.log("receipt.transactionHash: ", receipt.transactionHash);
     } else {
       console.log("No new vesting amounts to set for batch");
     }
-    console.log("batch: ", i);
     i++;
   }
+  console.log(`Total deduped: ${deduped.length} rows`);
+  const sumLiquid = deduped.reduce(
+    (sum, row) => (sum += BigInt(row.liquid_amount)),
+    0n
+  );
+  console.log("sumLiquid: ", formatEther(sumLiquid));
+
+  const sumVested = deduped.reduce(
+    (sum, row) => (sum += BigInt(row.vested_amount)),
+    0n
+  );
+  console.log("sumVested: ", formatEther(sumVested));
+
+  const content =
+    headerRow +
+    deduped
+      .map((row) => {
+        return `${row.user},${row.total_amount},${row.liquid_amount},${row.vested_amount}`;
+      })
+      .join("\n");
+  writeFileSync("deduped.csv", content);
 }
 
 main()
